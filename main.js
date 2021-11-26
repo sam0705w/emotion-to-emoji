5cc668b7f2f03a4d0b04408bb704cc898a6e2cdf
sam0705w <79701050+sam0705w@users.noreply.github.com>
2021-11-26 17:52:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ Webcam.set({
     image_format:"png",
     png_quality:90,
 })
-var webcam=document.getElementById("camera")
+var camera=document.getElementById("camera")
 
-Webcam.attach("#webcam")
+Webcam.attach("#camera")
 
 function capture(){
     Webcam.snap(function(data_uri){document.getElementById("result").innerHTML="<img src="+data_uri+">"})
