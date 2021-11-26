e94938c5346b2fd11cdf19812fdb4b4d747d8ccf
sam0705w <79701050+sam0705w@users.noreply.github.com>
2021-11-26 17:48:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var prediction_1=""
+var prediction_2=""
+
+Webcam.set({
+    width:350,
+    height:455,
+    image_format:"png",
+    png_quality:90,
+})
+var webcam=document.getElementById("camera")
+
+Webcam.attach("#webcam")
+
+function capture(){
+    Webcam.snap(function(data_uri){document.getElementById("result").innerHTML="<img src="+data_uri+">"})
+}
+
+function ttos(){
+    var symth=window.speechSynthesis
+    var text1= "First emotion is "+prediction_1 
+    var text2="and second emotion is"+prediction_2
+    var speech=new SpeechSynthesisUtterance(text1+text2)
+    symth.speak(speech)
+}
+console.log(ml5.version)
+var classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/UM9AI1pk4/model,json",modelLoaded)
+
+function modelLoaded(){
+    console.log("modelLoaded")
+}
